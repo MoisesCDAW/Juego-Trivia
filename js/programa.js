74592b01ccf496c6df3c818b6519cdd98446f9b4
MoisesCDAW <moisescamposdaw@gmail.com>
2024-10-06 14:17:09 +0100

    Reestructuración: 1. Se crean las preguntas de sesión con sus respuestas en index.html con un nuevo archivo "inicio.js" y se guardan en localStorage 2. Se obtienen e imprimen en preguntas.html con programa.js 3. La evaluación de las respuestas se evalúan en preguntas.html con programa.js 4. Se creó un resultados.js para imprimir el tiempo total empleado para responder

--- js/programa.js
+++ js/programa.js
@@ -1,44 +1,46 @@
-const preguntas = [
-    "¿Cuál es la flor nacional de Japón?",
-    "¿Cuál es el animal nacional de Australia?",
-    "¿Cómo se llamaba Estambul antes de 1923?",
-    "¿Cuál es el país más pequeño del mundo?",
-    "¿Cuál es el río más largo del mundo?",
-    "¿Cómo le llaman los locales a la Ciudad de Nueva York?",
-    "¿Cómo se llaman los androides principales de Star War?",
-    "¿Dónde se celebraron los primeros Juegos Olimpicos modernos?",
-    "¿Cuántos lados tiene un pentágono?",
-    "¿A qué país pertenece la Isla de Pascua?"
-];
 
+// VARIABLES
+let correcta, contadorPreguntas, cantidadPreguntas, finJuego;
 
-const respuestas = [
-    ["Flor de Cerezo", "Girasol", "Orquideas", "Margarita"],
-    ["Canguro", "León", "Hipopótamo", "Flamenco"],
-    ["Constantinopla", "Roma", "Madrid", "Hispania"],
-    ["El Vaticano", "Mónaco", "Puerto Rico", "Marbella"],
-    ["Río Nilo", "Río del Amazonas", "Río Rin", "Río Misisipi"],
-    ["Gotham", "Gohan", "City", "New Jersey"],
-    ["C3PO y R2D2", "OP3C y 2DR2", "C3PO y D2R2", "CTPO y R2D2"],
-    ["Atenas", "Roma", "Constantinopla", "Moscú"],
-    ["Cinco", "Díez", "Dos", "Cuatro"],
-    ["Chile", "EEUU", "China", "Indonesia"]
-];
-
-
-// VARIABLES Y CONSTANTES
-let numPregunta, correcta, cantidadPreguntas = 5, tiempoTotal; // El tiempo total se expresa en miliseg
-const inicioJuego = new Date(); // Se crea con la fecha actual
+// ARRAYS VARIABLES
+let resAleatorias = [], sesionRespuestas = [];
 
 
 // FUNCIONES DEL PROGRAMA
 
-function ordenAleatorio(array){
-    let arrayAux = [];
+function validarRespuesta(valor){
 
-    for (let i = 0; i < array[numPregunta].length; i++) {
-        let indiceAleatorio = Math.floor(Math.random() * (array[numPregunta].length - 0) + 0);
-        let valor = array[numPregunta][indiceAleatorio];
+    if (valor.value==correcta) {
+        alert("Respuesta CORRECTA");
+    }else {
+        alert("Respuesta INCORRECTA");
+    }
+
+    cantidadPreguntas--;
+    contadorPreguntas++;
+
+    if (cantidadPreguntas>-1) {
+        localStorage.setItem("cantidadPreguntas", cantidadPreguntas);
+        localStorage.setItem("contadorPreguntas", contadorPreguntas);
+        window.open("preguntas.html", "_self");
+
+    }else{
+        finJuego = new Date().getTime(); // Se crea con la fecha actual  
+        localStorage.setItem("tiempoFin", finJuego); // Devuelve el resultado en miliseg
+        
+        window.open("resultados.html", "_self");
+    }
+    
+}
+
+
+function ordenAleatorio(arrayRespuestas){
+    let arrayAux = [];
+    let indiceAleatorio, valor;
+
+    for (let i = 0; i < arrayRespuestas.length; i++) {
+        indiceAleatorio = Math.floor(Math.random() * (arrayRespuestas.length - 0) + 0);
+        valor = arrayRespuestas[indiceAleatorio];
 
         if (arrayAux.indexOf(valor)==-1) {
             arrayAux[i] = valor;
@@ -51,41 +53,19 @@ function ordenAleatorio(array){
 }
 
 
-function validar(valor){
+function inicio() {
+    cantidadPreguntas = localStorage.getItem("cantidadPreguntas");
+    contadorPreguntas = localStorage.getItem("contadorPreguntas");
 
-    if (valor.value==correcta) {
-        alert("Respuesta CORRECTA");
-    }else {
-        alert("Respuesta INCORRECTA");
+    for (let i = 0; i < 4; i++) {
+        sesionRespuestas.push(localStorage.getItem("respuestas"+contadorPreguntas+"."+i));
     }
 
-    cantidadPreguntas--;
-    if (cantidadPreguntas!=0) {
-        selectPregunta();
-    }else{
-        const finJuego = new Date(); // Se crea con la fecha actual       
-        tiempoTotal = finJuego - inicioJuego; // Devuelve el resultado en miliseg
+    correcta = sesionRespuestas[0];
 
-        if (tiempoTotal<60000) { // Si es menor a un minuto el mensaje será en seg
-            // console.log("Tardes " + (tiempoTotal/1000).toFixed(2) +" segundo(s) en responder");
-        }else {
-            // console.log("Tiempo " + (tiempoTotal/60000).toFixed(2) +" minuto(s) en responder"); // Si no, será en min
-        }
-        
-        // window.open("resultados.html", "_self");
-    }
-    
-}
+    resAleatorias = ordenAleatorio(sesionRespuestas);
 
-
-function selectPregunta(){
-
-    numPregunta = Math.floor(Math.random() * (preguntas.length - 0) + 0);
-    let pregunta = preguntas[numPregunta];
-    correcta = respuestas[numPregunta][0];
-    let resAleatorias = ordenAleatorio(respuestas);
-
-    document.getElementById("pregunta").innerHTML = pregunta;
+    document.getElementById("pregunta").innerHTML = localStorage.getItem("pregunta"+contadorPreguntas); 
 
     for (let i = 0; i < resAleatorias.length; i++) {
         document.getElementById("boton"+(i+1)).innerHTML = resAleatorias[i];
@@ -93,4 +73,4 @@ function selectPregunta(){
     }
 }
 
-selectPregunta();
+inicio();
